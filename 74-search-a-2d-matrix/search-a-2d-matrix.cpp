c2d7aa707ad622c2265fef8f0f269c090321f3ad
Shubham0247 <143329730+Shubham0247@users.noreply.github.com>
2024-02-04 00:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (13.29%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,30 +3,17 @@ public:
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-		// if matrix have 0 rows or 0 colums
-        if(n == 0 || m == 0)
-            return false;
-        
-		// treating matrix as array just taking care of indices
-		// [0..n*m]
-        int start = 0, end = m*n - 1;
-        
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-			// a[x] : matrix[x / m][x % m] formulae
-            int ind = matrix[mid/m][mid%m];
-            if (target == ind)
-                return true;
-			// left half
-            else if(target < ind)
-                end = mid - 1;
-            else
-			// right half
-                start = mid + 1;       
+       int m = matrix.size(), n = matrix[0].size();
+        int left = 0, right = m*n-1;
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+            int midNum = matrix[mid/n][mid%n];
+            if (target==midNum) return true;
+            else if (target < midNum) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
         return false;
     }
