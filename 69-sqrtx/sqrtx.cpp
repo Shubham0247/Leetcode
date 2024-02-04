0f713433a45d5769e108c08f7d7d78b0e98d1a31
Shubham0247 <143329730+Shubham0247@users.noreply.github.com>
2024-02-04 21:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (11.33%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    int sqrtBinarySearch(int x) {
+
+    if (x == 0)
+        return 0;
+    int left = 1, right = INT_MAX;
+
+    while (true) {
+        int mid = left + (right - left)/2;
+        if (mid > x/mid) {
+            right = mid - 1;
+        } else {
+            if (mid + 1 > x/(mid + 1))
+                return mid;
+            left = mid + 1;
+        }
+    }
+    }
+
+    int mySqrt(int x) {
+        return sqrtBinarySearch(x);
+    }
+};
